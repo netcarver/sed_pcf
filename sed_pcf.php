24ed9b12423c574f212c7668aa295a2dabcdb94a
netcarver <nc.github@netcarving.com>
2007-12-19 16:53:55 +0000

    * Added new tag: sed_pcf_for_each_value. * Updated help section

--- sed_pcf.php
+++ sed_pcf.php
@@ -51,12 +51,13 @@ This little plugin allows you to use an article's custom fields to store multipl
 h2(#tags). Tag Directory
 
 |_. Tag    |_. Description  |
-| "<code>sed_pcf_get_value</code>":#get | Returns the named value from the named section of the named custom field. |
-| "<code>sed_pcf_if_value</code>":#if | Tests for the existence of a named value in a named section of a custom field. |
-| "<code>sed_pcf_if_field_section</code>":#ifsection | Tests for the existence of a named section of a custom field. |
-| "<code>sed_pcf_email</code>":#email | Uses the values in a named section of a custom field to call the TXP email tag. |
-| "<code>sed_pcf_image</code>":#image | Uses the values in a named section of a custom field to call the TXP image tag. |
-| "<code>sed_pcf_thumbnail</code>":#thumb | Uses the values in a named section of a custom field to call the TXP thumbnail tag. |
+| "sed_pcf_get_value":#get | Returns the named value from the named section of the named custom field. |
+| "sed_pcf_if_value":#if | Tests for the existence of a named value in a named section of a custom field. |
+| "sed_pcf_if_field_section":#ifsection | Tests for the existence of a named section of a custom field. |
+| "sed_pcf_email":#email | Uses the values in a named section of a custom field to call the TXP email tag. |
+| "sed_pcf_image":#image | Uses the values in a named section of a custom field to call the TXP image tag. |
+| "sed_pcf_thumbnail":#thumb | Uses the values in a named section of a custom field to call the TXP thumbnail tag. |
+| "sed_pcf_for_each_value":#foreach | Uses the values in a named section of a custom field to call the TXP thumbnail tag. |
 
 h2(#get). The @sed_pcf_get_value@ tag
 
@@ -119,9 +120,42 @@ h2(#thumb). The @sed_pcf_thumbnail@ tag
 
 As for images, so with thumbnails!
 
-h2(#format). Field Format
+h2(#foreach). The @sed_pcf_for_each_value@ tag
 
-There are two formats you can use. A simple and a normal format.
+This can be used as an enclosing tag, or with a form, or as a single tag. It was inspired by the *wet_foreach_image* plugin and works in a very similar manner.
+
+|_. Attribute    |_. Default Value |_. Status   |_. Description |
+| 'name'         | 'custom1'       | Needed   | Name of the custom field to access. |
+| 'debug'        | 0               | Optional | Set to 1 to output extra debug info |
+| 'form'         | ''              | Optional | Form to use to format output |
+| 'label'        | ''              | Optional | Label text for the output |
+| 'labeltag'     | ''              | Optional | tag for the label |
+| 'wraptag'      | 'ul'            | Optional | Tag to wrap the whole output in -- you might want to set this to '' if using a form. |
+| 'break'        | 'li'            | Optional | Tag to wrap each individual value's output in -- again, you may want to set this to '' if your form wraps the output. |
+| 'class'        | 'custom1'       | Optional | Class to apply to the wrapped output |
+
+This tag can be used in three ways...
+
+# As a single tag with default layout... @<txp:sed_pcf_for_each_value />@
+# As a single tag using a form for layout... @<txp:sed_pcf_for_each_value form="your_form" />@
+# As an enclosing tag... @<txp:sed_pcf_for_each_value>... your stuff here...<txp:sed_pcf_for_each_value />@
+
+The enclosed section, default or supplied form will get parsed once per value in the custom field's list.
+
+On each parse, the string @'{value}'@ will be replaced by the corresponding value from the custom field.
+
+The default layout, used in the absence of a form or contained section, is '{value}'.
+
+h2(#format). Field Formats
+
+There are three formats you can use in your custom fields.
+
+h3(#foreachformat). ForEach Format.
+
+This format can only be used by the @sed_pcf_for_each_value()@ tag. It's a simple comma seperated list of values...
+
+* a,b,c,d,g
+
 
 h3(#simple). Simple Format
 
@@ -141,7 +175,13 @@ Some examples of valid fields...
 * section1(a='1';b='2')|section2(c='Hello!';d='';e='Goodbye!')
 * copyright(start='1945';owner='Ritchard the great')|email(email='john@nowhere.splat';linktext='link text';title='Click here to send a message')
 
+
+
 h2. Version History.
+
+v0.3 Implemented the following features&#8230;
+
+* Added the @sed_pcf_for_each_value()@ tag.
 
 v0.2 Implemented the following features&#8230;
 
@@ -308,6 +348,52 @@ function sed_pcf_thumbnail( $atts ) {
 function sed_pcf_email( $atts ) {
 	$atts['txp_fn'] = 'email';
 	return _sed_pcf_txp_fn( $atts );
+	}
+
+function sed_pcf_for_each_value( $atts , $thing )
+	{
+	global $thisarticle;
+
+	assert_article();
+
+	$def_custom_name = 'custom1';
+	extract( $merged = lAtts( array(
+		'debug'		=> 0,
+		'name'	=> $def_custom_name,
+		'form'  	=> '',
+		'label'  	=> '',
+		'labeltag'  => '',
+		'wraptag'	=> 'ul',
+		'break'		=> 'li',
+		'class'		=> '',
+		), $atts) );
+
+	if( $debug ) echo dmp( $merged );
+
+	$field = @$thisarticle[$name];
+	if( empty( $field ) )	# Nothing to do -- the field is empty.
+		{
+		if( $debug ) echo "Returning early - nothing to do in CF[$name].";
+		return '';
+		}
+
+	if( empty( $class ) )
+		$class = $name;
+
+	if( !empty( $form ) )		# grab the form (if any)
+		$thing = fetch_form($form);
+
+	if( empty( $thing ) )		# if no form, and no enclosed thing, use built-in formula...
+		$thing = '{value}';
+
+	$out = array();
+	$field = do_list( $field );
+	foreach( $field as $value )
+		{
+		$out[] = parse( str_replace( '{value}' , $value , $thing ) );
+		}
+
+	return doLabel($label, $labeltag).doWrap($out, $wraptag, $break, $class);
 	}
 
 # --- END PLUGIN CODE ---
